94dcc82e61b7a0eb84ed02ac6e99a7437b9f806d
Pablo Irungaray <pabloirungaray@gmail.com>
2026-07-15 14:43:00 -0600

    cambios en seccion de mensajes

--- routes/messages.js
+++ routes/messages.js
@@ -36,7 +36,7 @@ router.get('/', async (req,res) => {
             mensajes m
         join 
             usuarios u  on u.id_usuario  = m.id_emisor
-        where m.id_receptor =${id_user} order by m.horafecha desc;
+        where m.id_receptor =${id_user} or m.id_emisor= ${id_user} order by m.horafecha desc;
         `;
         console.log(strQuery)
 
